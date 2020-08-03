9d776d70c17e97a605079b50b3e58e50e0d4cff7
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2020-08-03 11:20:20 +0900

    Update repository.ts

--- store/moviediscover/repository.ts
+++ store/moviediscover/repository.ts
@@ -8,6 +8,6 @@ export const movieApi = axios.create({
 });
 
 export async function fetchMovieDisc(): Promise<Result> {
-  const response = await movieApi.get('/discover/movie?api_key=95e87c5acbcfa3a1b0d4df3e1a73755b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+  const response = await movieApi.get('/discover/movie?api_key={PUT_YOUR_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
   return response.data.result as Result
 }
