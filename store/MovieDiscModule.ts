f13884675b1458d4ef34d9ec59375f1870a5edf9
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2020-08-03 11:19:02 +0900

    Update MovieDiscModule.ts

--- store/MovieDiscModule.ts
+++ store/MovieDiscModule.ts
@@ -10,7 +10,7 @@ class MovieDiscModule extends VuexModule {
 
     @MutationAction
     async getMovieDisc() {
-    const response = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=95e87c5acbcfa3a1b0d4df3e1a73755b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+    const response = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key={PUT_YOUR_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
     const list = response.data.results
     return { list }
     }
